257aa21942b36a6a51c8be6f8d87857b25e1c436
Denilson <adcdenis@gmail.com>
2025-10-30 18:34:17 -0300

    aumento icones menu

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -82,7 +82,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
                           : 'text-gray-500 hover:text-gray-700 hover:border-gray-300'
                       }`}
                     >
-                      <Icon className="h-4 w-4 sm:h-4 sm:w-4 mr-1 sm:mr-2 flex-shrink-0" />
+                      <Icon className="h-5 w-5 sm:h-4 sm:w-4 mr-1 sm:mr-2 flex-shrink-0" />
                       <span className="hidden sm:inline truncate">{item.name}</span>
                     </Link>
                   );
